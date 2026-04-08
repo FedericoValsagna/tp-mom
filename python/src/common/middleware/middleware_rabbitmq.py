929f4ee2e71e523855a4c3019788b3c7e84993f5
Federico Valsagna <fvalsagna@fi.uba.ar>
2026-04-08 01:20:04 -0300

    All tests pass

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -45,23 +45,26 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
         self.connection = pika.BlockingConnection(
         pika.ConnectionParameters(host=host))
         self.channel = self.connection.channel()
-        self.channel.exchange_declare(exchange=self.exchange_name, exchange_type='fanout')
+        self.channel.exchange_declare(exchange=self.exchange_name, exchange_type='direct')
         
     def start_consuming(self, on_message_callback):
         def callback(ch, method, properties, body):
             on_message_callback(body, ack = lambda:  ch.basic_ack(delivery_tag = method.delivery_tag), nack = lambda: ch.basic_nack(delivery_tag = method.delivery_tag))
         result = self.channel.queue_declare(queue='', exclusive=True)
         queue_name = result.method.queue
-        self.channel.queue_bind(exchange=self.exchange_name, queue=queue_name)
-        self.channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+        # Tal vez deban ser multiples queues
+        for routing_key in self.routing_keys:
+            self.channel.queue_bind(exchange=self.exchange_name, queue=queue_name, routing_key=routing_key)
+        self.channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=False)
         self.channel.start_consuming()
     
 
     def stop_consuming(self):
-        return super().stop_consuming()
+        self.channel.close()
+        self.connection.close()
     
     def send(self, message):
-        self.channel.basic_publish(exchange=self.exchange_name, routing_key=self.routing_keys, body=message)
+        self.channel.basic_publish(exchange=self.exchange_name, routing_key=self.routing_keys[0], body=message)
 
     def close(self):
         if self.channel != None:
